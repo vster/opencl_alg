f5cc4865249d603a8d8b927437d25d765df6a286
Vital Teresh <vster2006@gmail.com>
2017-05-03 21:42:48 +0300

    mod blowfish.cpp

--- blowfish/blowfish.cpp
+++ blowfish/blowfish.cpp
@@ -14,15 +14,34 @@ void Blowfish::encrypt(const byte in[BLOCKSIZE], byte out[BLOCKSIZE]) const
      {
    u32bit L  = make_u32bit(in[0], in[1], in[2], in[3]),
           R = make_u32bit(in[4], in[5], in[6], in[7]);
-   round(L, R, 0); round(R, L, 1); round(L, R, 2); round(R, L, 3);
-   round(L, R, 4); round(R, L, 5); round(L, R, 6); round(R, L, 7);
-   round(L, R, 8); round(R, L, 9); round(L, R,10); round(R, L,11);
-   round(L, R,12); round(R, L,13); round(L, R,14); round(R, L,15);
-   L ^= Pbox[16]; R ^= Pbox[17];
-   out[0] = get_byte(0, R); out[1] = get_byte(1, R);
-   out[2] = get_byte(2, R); out[3] = get_byte(3, R);
-   out[4] = get_byte(0, L); out[5] = get_byte(1, L);
-   out[6] = get_byte(2, L); out[7] = get_byte(3, L);
+   round(L, R, 0);
+   round(R, L, 1);
+   round(L, R, 2);
+   round(R, L, 3);
+   round(L, R, 4);
+   round(R, L, 5);
+   round(L, R, 6);
+   round(R, L, 7);
+   round(L, R, 8);
+   round(R, L, 9);
+   round(L, R,10);
+   round(R, L,11);
+   round(L, R,12);
+   round(R, L,13);
+   round(L, R,14);
+   round(R, L,15);
+
+   L ^= Pbox[16];
+   R ^= Pbox[17];
+
+   out[0] = get_byte(0, R);
+   out[1] = get_byte(1, R);
+   out[2] = get_byte(2, R);
+   out[3] = get_byte(3, R);
+   out[4] = get_byte(0, L);
+   out[5] = get_byte(1, L);
+   out[6] = get_byte(2, L);
+   out[7] = get_byte(3, L);
    }
 
 /*************************************************
@@ -32,15 +51,34 @@ void Blowfish::decrypt(const byte in[BLOCKSIZE], byte out[BLOCKSIZE]) const
    {
    u32bit L  = make_u32bit(in[0], in[1], in[2], in[3]),
           R = make_u32bit(in[4], in[5], in[6], in[7]);
-   round(L, R,17); round(R, L,16); round(L, R,15); round(R, L,14);
-   round(L, R,13); round(R, L,12); round(L, R,11); round(R, L,10);
-   round(L, R, 9); round(R, L, 8); round(L, R, 7); round(R, L, 6);
-   round(L, R, 5); round(R, L, 4); round(L, R, 3); round(R, L, 2);
-   L ^= Pbox[1]; R ^= Pbox[0];
-   out[0] = get_byte(0, R); out[1] = get_byte(1, R);
-   out[2] = get_byte(2, R); out[3] = get_byte(3, R);
-   out[4] = get_byte(0, L); out[5] = get_byte(1, L);
-   out[6] = get_byte(2, L); out[7] = get_byte(3, L);
+   round(L, R,17);
+   round(R, L,16);
+   round(L, R,15);
+   round(R, L,14);
+   round(L, R,13);
+   round(R, L,12);
+   round(L, R,11);
+   round(R, L,10);
+   round(L, R, 9);
+   round(R, L, 8);
+   round(L, R, 7);
+   round(R, L, 6);
+   round(L, R, 5);
+   round(R, L, 4);
+   round(L, R, 3);
+   round(R, L, 2);
+
+   L ^= Pbox[1];
+   R ^= Pbox[0];
+
+   out[0] = get_byte(0, R);
+   out[1] = get_byte(1, R);
+   out[2] = get_byte(2, R);
+   out[3] = get_byte(3, R);
+   out[4] = get_byte(0, L);
+   out[5] = get_byte(1, L);
+   out[6] = get_byte(2, L);
+   out[7] = get_byte(3, L);
    }
 
 /*************************************************
